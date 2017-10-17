664a26309e54ea4032885fc589209b083873698a
webeweb <support@webeweb.fr>
2017-10-17 19:00:30 +0200

    Add main bundle class

---
+++ WBWjQueryQueryBuilderBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the WBWJQueryQueryBuilderBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\JQuery\QueryBuilderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * jQuery QueryBuilder bundle.
+ *
+ * @author WBW <https://github.com/webeweb/WBWJQueryQueryBuilderBundle>
+ * @package WBW\JQuery\QueryBuilderBundle
+ * @version 2.4.3
+ * @final
+ */
+final class WBWJQueryQueryBuilderBundle extends Bundle {
+
+}
