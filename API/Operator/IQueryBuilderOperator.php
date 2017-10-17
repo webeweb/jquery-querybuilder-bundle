dca3360456cabdb792445281701c6c8688a36cad
webeweb <support@webeweb.fr>
2017-10-17 19:05:10 +0200

    Add QueryBuilder operator

---
+++ API/Operator/IQueryBuilderOperator.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * This file is part of the WBWJQueryQueryBuilderBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\JQuery\QueryBuilderBundle\API\Operator;
+
+/**
+ * QueryBuilder operator interface.
+ *
+ * @author WBW <https://github.com/webeweb/WBWJQueryQueryBuilderBundle>
+ * @package WBW\JQuery\QueryBuilderBundle\API\Operator
+ * @version 2.4.3
+ * @final
+ */
+interface IQueryBuilderOperator {
+
+    /**
+     * Operators.
+     */
+    const OPERATORS = [
+        self::OPERATOR_BEGINS_WITH      => "LIKE",
+        self::OPERATOR_BETWEEN          => "BETWEEN",
+        self::OPERATOR_CONTAINS         => "LIKE",
+        self::OPERATOR_ENDS_WITH        => "LIKE",
+        self::OPERATOR_EQUAL            => "=",
+        self::OPERATOR_GREATER          => ">",
+        self::OPERATOR_GREATER_OR_EQUAL => ">=",
+        self::OPERATOR_IN               => "IN",
+        self::OPERATOR_IS_EMPTY         => "IS NULL",
+        self::OPERATOR_IS_NOT_EMPTY     => "IS NOT NULL",
+        self::OPERATOR_IS_NOT_NULL      => "IS NOT NULL",
+        self::OPERATOR_IS_NULL          => "IS NULL",
+        self::OPERATOR_LESS             => "<",
+        self::OPERATOR_LESS_OR_EQUAL    => "<=",
+        self::OPERATOR_NOT_BEGINS_WITH  => "NOT LIKE",
+        self::OPERATOR_NOT_BETWEEN      => "NOT BETWEEN",
+        self::OPERATOR_NOT_CONTAINS     => "NOT LIKE",
+        self::OPERATOR_NOT_ENDS_WITH    => "NOT LIKE",
+        self::OPERATOR_NOT_EQUAL        => "<>",
+        self::OPERATOR_NOT_IN           => "NOT IN",
+    ];
+
+    /**
+     * Operator begins with.
+     */
+    const OPERATOR_BEGINS_WITH = "begins_with";
+
+    /**
+     * Operator between.
+     */
+    const OPERATOR_BETWEEN = "between";
+
+    /**
+     * Operator contains.
+     */
+    const OPERATOR_CONTAINS = "contains";
+
+    /**
+     * Operator ends with.
+     */
+    const OPERATOR_ENDS_WITH = "ends_with";
+
+    /**
+     * Operator equal.
+     */
+    const OPERATOR_EQUAL = "equal";
+
+    /**
+     * Operator greater.
+     */
+    const OPERATOR_GREATER = "greater";
+
+    /**
+     * Operator greater or equal.
+     */
+    const OPERATOR_GREATER_OR_EQUAL = "greater_or_equal";
+
+    /**
+     * Operator in.
+     */
+    const OPERATOR_IN = "in";
+
+    /**
+     * Operator less.
+     */
+    const OPERATOR_LESS = "less";
+
+    /**
+     * Operator less or equal.
+     */
+    const OPERATOR_LESS_OR_EQUAL = "less_or_equal";
+
+    /**
+     * Operator not begins with.
+     */
+    const OPERATOR_NOT_BEGINS_WITH = "not_begins_with";
+
+    /**
+     * Operator not between.
+     */
+    const OPERATOR_NOT_BETWEEN = "not_between";
+
+    /**
+     * Operator not contains.
+     */
+    const OPERATOR_NOT_CONTAINS = "not_contains";
+
+    /**
+     * Operator not ends with.
+     */
+    const OPERATOR_NOT_ENDS_WITH = "not_ends_with";
+
+    /**
+     * Operator not_equal.
+     */
+    const OPERATOR_NOT_EQUAL = "not_equal";
+
+    /**
+     * Operator not in.
+     */
+    const OPERATOR_NOT_IN = "not_in";
+
+    /**
+     * Operator is empty.
+     */
+    const OPERATOR_IS_EMPTY = "is_empty";
+
+    /**
+     * Operator is not empty.
+     */
+    const OPERATOR_IS_NOT_EMPTY = "is_not_empty";
+
+    /**
+     * Operator is not null.
+     */
+    const OPERATOR_IS_NOT_NULL = "is_not_null";
+
+    /**
+     * Operator is null.
+     */
+    const OPERATOR_IS_NULL = "is_null";
+
+}
---
+++ Tests/API/Operator/IQueryBuilderOperatorTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of the WBWJQueryQueryBuilderBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\JQuery\QueryBuilderBundle\Tests\API\Operator;
+
+use PHPUnit_Framework_TestCase;
+use WBW\JQuery\QueryBuilderBundle\API\Operator\IQueryBuilderOperator;
+
+/**
+ * QueryBuilder operator interface test.
+ *
+ * @author WBW <https://github.com/webeweb/WBWJQueryQueryBuilderBundle>
+ * @package WBW\JQuery\QueryBuilderBundle\Tests\API\Operator
+ * @version 2.4.3
+ * @final
+ */
+final class IQueryBuilderOperatorTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $this->assertEquals("begins_with", IQueryBuilderOperator::OPERATOR_BEGINS_WITH, "The constant OPERATOR_BEGINS_WITH does not return the expected value");
+        $this->assertEquals("between", IQueryBuilderOperator::OPERATOR_BETWEEN, "The constant OPERATOR_BETWEEN does not return the expected value");
+        $this->assertEquals("contains", IQueryBuilderOperator::OPERATOR_CONTAINS, "The constant OPERATOR_CONTAINS does not return the expected value");
+        $this->assertEquals("ends_with", IQueryBuilderOperator::OPERATOR_ENDS_WITH, "The constant OPERATOR_ENDS_WITH does not return the expected value");
+        $this->assertEquals("equal", IQueryBuilderOperator::OPERATOR_EQUAL, "The constant OPERATOR_EQUAL does not return the expected value");
+        $this->assertEquals("greater", IQueryBuilderOperator::OPERATOR_GREATER, "The constant OPERATOR_GREATER does not return the expected value");
+        $this->assertEquals("greater_or_equal", IQueryBuilderOperator::OPERATOR_GREATER_OR_EQUAL, "The constant OPERATOR_GREATER_OR_EQUAL does not return the expected value");
+        $this->assertEquals("in", IQueryBuilderOperator::OPERATOR_IN, "The constant OPERATOR_IN does not return the expected value");
+        $this->assertEquals("is_empty", IQueryBuilderOperator::OPERATOR_IS_EMPTY, "The constant OPERATOR_IS_EMPTY does not return the expected value");
+        $this->assertEquals("is_not_empty", IQueryBuilderOperator::OPERATOR_IS_NOT_EMPTY, "The constant OPERATOR_IS_NOT_EMPTY does not return the expected value");
+        $this->assertEquals("is_not_null", IQueryBuilderOperator::OPERATOR_IS_NOT_NULL, "The constant OPERATOR_IS_NOT_NULL does not return the expected value");
+        $this->assertEquals("is_null", IQueryBuilderOperator::OPERATOR_IS_NULL, "The constant OPERATOR_IS_NULL does not return the expected value");
+        $this->assertEquals("less", IQueryBuilderOperator::OPERATOR_LESS, "The constant OPERATOR_LESS does not return the expected value");
+        $this->assertEquals("less_or_equal", IQueryBuilderOperator::OPERATOR_LESS_OR_EQUAL, "The constant OPERATOR_LESS_OR_EQUAL does not return the expected value");
+        $this->assertEquals("not_begins_with", IQueryBuilderOperator::OPERATOR_NOT_BEGINS_WITH, "The constant OPERATOR_NOT_BEGINS_WITH does not return the expected value");
+        $this->assertEquals("not_between", IQueryBuilderOperator::OPERATOR_NOT_BETWEEN, "The constant OPERATOR_NOT_BETWEEN does not return the expected value");
+        $this->assertEquals("not_contains", IQueryBuilderOperator::OPERATOR_NOT_CONTAINS, "The constant OPERATOR_NOT_CONTAINS does not return the expected value");
+        $this->assertEquals("not_ends_with", IQueryBuilderOperator::OPERATOR_NOT_ENDS_WITH, "The constant OPERATOR_NOT_ENDS_WITH does not return the expected value");
+        $this->assertEquals("not_equal", IQueryBuilderOperator::OPERATOR_NOT_EQUAL, "The constant OPERATOR_NOT_EQUAL does not return the expected value");
+        $this->assertEquals("not_in", IQueryBuilderOperator::OPERATOR_NOT_IN, "The constant OPERATOR_NOT_IN does not return the expected value");
+
+        $this->assertEquals("LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_BEGINS_WITH], "The constant OPERATORS[OPERATOR_BEGINS_WITH] does not return the expected value");
+        $this->assertEquals("BETWEEN", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_BETWEEN], "The constant OPERATORS[OPERATOR_BETWEEN] does not return the expected value");
+        $this->assertEquals("LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_CONTAINS], "The constant OPERATORS[OPERATOR_CONTAINS] does not return the expected value");
+        $this->assertEquals("LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_ENDS_WITH], "The constant OPERATORS[OPERATOR_ENDS_WITH] does not return the expected value");
+        $this->assertEquals("=", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_EQUAL], "The constant OPERATORS[OPERATOR_EQUAL] does not return the expected value");
+        $this->assertEquals(">", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_GREATER], "The constant OPERATORS[OPERATOR_GREATER] does not return the expected value");
+        $this->assertEquals(">=", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_GREATER_OR_EQUAL], "The constant OPERATORS[OPERATOR_GREATER_OR_EQUAL] does not return the expected value");
+        $this->assertEquals("IN", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_IN], "The constant OPERATORS[OPERATOR_IN] does not return the expected value");
+        $this->assertEquals("IS NULL", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_IS_EMPTY], "The constant OPERATORS[OPERATOR_IS_EMPTY] does not return the expected value");
+        $this->assertEquals("IS NOT NULL", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_IS_NOT_EMPTY], "The constant OPERATORS[OPERATOR_IS_NOT_EMPTY] does not return the expected value");
+        $this->assertEquals("IS NOT NULL", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_IS_NOT_NULL], "The constant OPERATORS[OPERATOR_IS_NOT_NULL] does not return the expected value");
+        $this->assertEquals("IS NULL", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_IS_NULL], "The constant OPERATORS[OPERATOR_IS_NULL] does not return the expected value");
+        $this->assertEquals("<", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_LESS], "The constant OPERATORS[OPERATOR_LESS] does not return the expected value");
+        $this->assertEquals("<=", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_LESS_OR_EQUAL], "The constant OPERATORS[OPERATOR_LESS_OR_EQUAL] does not return the expected value");
+        $this->assertEquals("NOT LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_BEGINS_WITH], "The constant OPERATORS[OPERATOR_NOT_BEGINS_WITH] does not return the expected value");
+        $this->assertEquals("NOT BETWEEN", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_BETWEEN], "The constant OPERATORS[OPERATOR_NOT_BETWEEN] does not return the expected value");
+        $this->assertEquals("NOT LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_CONTAINS], "The constant OPERATORS[OPERATOR_NOT_CONTAINS] does not return the expected value");
+        $this->assertEquals("NOT LIKE", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_ENDS_WITH], "The constant OPERATORS[OPERATOR_NOT_ENDS_WITH] does not return the expected value");
+        $this->assertEquals("<>", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_EQUAL], "The constant OPERATORS[OPERATOR_NOT_EQUAL] does not return the expected value");
+        $this->assertEquals("NOT IN", IQueryBuilderOperator::OPERATORS[IQueryBuilderOperator::OPERATOR_NOT_IN], "The constant OPERATORS[OPERATOR_NOT_IN] does not return the expected value");
+    }
+
+}
