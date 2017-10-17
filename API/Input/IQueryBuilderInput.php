6e1c169564904879c2101a3426887b5b0e50bdcc
webeweb <support@webeweb.fr>
2017-10-17 19:04:55 +0200

    Add QueryBuilder input

---
+++ API/Input/IQueryBuilderInput.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the WBWJQueryQueryBuilderBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\JQuery\QueryBuilderBundle\API\Input;
+
+/**
+ * QueryBuilder input interface.
+ *
+ * @author WBW <https://github.com/webeweb/WBWJQueryQueryBuilderBundle>
+ * @package WBW\JQuery\QueryBuilderBundle\API\Input
+ * @version 2.4.3
+ * @final
+ */
+interface IQueryBuilderInput {
+
+    /**
+     * Inputs.
+     */
+    const INPUTS = [
+        self::INPUT_CHECKBOX => self::INPUT_CHECKBOX,
+        self::INPUT_NUMBER   => self::INPUT_NUMBER,
+        self::INPUT_RADIO    => self::INPUT_RADIO,
+        self::INPUT_SELECT   => self::INPUT_SELECT,
+        self::INPUT_TEXT     => self::INPUT_TEXT,
+        self::INPUT_TEXTAREA => self::INPUT_TEXTAREA,
+    ];
+
+    /**
+     * Input checkbox.
+     */
+    const INPUT_CHECKBOX = "checkbox";
+
+    /**
+     * Input number.
+     */
+    const INPUT_NUMBER = "number";
+
+    /**
+     * Input radio.
+     */
+    const INPUT_RADIO = "radio";
+
+    /**
+     * Input select.
+     */
+    const INPUT_SELECT = "select";
+
+    /**
+     * Input text.
+     */
+    const INPUT_TEXT = "text";
+
+    /**
+     * Input textarea.
+     */
+    const INPUT_TEXTAREA = "textarea";
+
+}
---
+++ Tests/API/Input/IQueryBuilderInputTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the WBWJQueryQueryBuilderBundle package.
+ *
+ * (c) 2017 WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\JQuery\QueryBuilderBundle\Tests\API\Input;
+
+use PHPUnit_Framework_TestCase;
+use WBW\JQuery\QueryBuilderBundle\API\Input\IQueryBuilderInput;
+
+/**
+ * QueryBuilder input interface.
+ *
+ * @author WBW <https://github.com/webeweb/WBWJQueryQueryBuilderBundle>
+ * @package WBW\JQuery\QueryBuilderBundle\Tests\API\Input
+ * @version 2.4.3
+ * @final
+ */
+final class IQueryBuilderInputTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Test the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstructor() {
+
+        $this->assertEquals("checkbox", IQueryBuilderInput::INPUT_CHECKBOX, "The constant INPUT_CHECKBOX does not return the expected value");
+        $this->assertEquals("number", IQueryBuilderInput::INPUT_NUMBER, "The constant INPUT_NUMBER does not return the expected value");
+        $this->assertEquals("radio", IQueryBuilderInput::INPUT_RADIO, "The constant INPUT_RADIO does not return the expected value");
+        $this->assertEquals("select", IQueryBuilderInput::INPUT_SELECT, "The constant INPUT_SELECT does not return the expected value");
+        $this->assertEquals("text", IQueryBuilderInput::INPUT_TEXT, "The constant INPUT_TEXT does not return the expected value");
+        $this->assertEquals("textarea", IQueryBuilderInput::INPUT_TEXTAREA, "The constant INPUT_TEXTAREA does not return the expected value");
+    }
+
+}
