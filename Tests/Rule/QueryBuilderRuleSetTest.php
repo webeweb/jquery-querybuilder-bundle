9ef05f4a7a51b60e92c2a59cca93c71efd4fed95
webeweb <support@webeweb.fr>
2017-10-25 16:11:51 +0200

    Update QueryBuilder rule set test

--- Tests/Rule/QueryBuilderRuleSetTest.php
+++ Tests/Rule/QueryBuilderRuleSetTest.php
@@ -12,6 +12,7 @@
 namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Rule;
 
 use PHPUnit_Framework_TestCase;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Rule\QueryBuilderRule;
 use WBW\Bundle\JQuery\QueryBuilderBundle\Rule\QueryBuilderRuleSet;
 
 /**
@@ -25,6 +26,8 @@ final class QueryBuilderRuleSetTest extends PHPUnit_Framework_TestCase {
 
     /**
      * Tests the __construct() method.
+     *
+     * @return void
      */
     public function testConstruct() {
 
@@ -33,6 +36,34 @@ final class QueryBuilderRuleSetTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(null, $obj->getCondition(), "The method getCondition() does not return the expected value");
         $this->assertEquals([], $obj->getRules(), "The method getRules() does not return the expected value");
         $this->assertEquals(false, $obj->getValid(), "The method getRules() does not return the expected value");
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        $rules = [
+            "condition" => "OR",
+            "rules"     => [
+                ["id" => "age", "field" => "age", "input" => QueryBuilderRule::INPUT_NUMBER, "operator" => QueryBuilderRule::OPERATOR_GREATER, "type" => QueryBuilderRule::TYPE_INTEGER, "value" => "21"],
+                [
+                    "condition" => "AND",
+                    "rules"     => [
+                        ["id" => "firstname", "field" => "firstname", "input" => QueryBuilderRule::INPUT_TEXT, "operator" => QueryBuilderRule::OPERATOR_EQUAL, "type" => QueryBuilderRule::TYPE_STRING, "value" => "John"],
+                        ["id" => "lastname", "field" => "lastname", "input" => QueryBuilderRule::INPUT_NUMBER, "operator" => QueryBuilderRule::OPERATOR_EQUAL, "type" => QueryBuilderRule::TYPE_STRING, "value" => "DOE"],
+                    ],
+                ],
+            ],
+            "valid"     => true,
+        ];
+
+        $obj = new QueryBuilderRuleSet($rules, null);
+
+        $res = "(age > 21 OR (firstname = 'John' AND lastname = 'DOE'))";
+        $this->assertEquals($res, $obj->toSQL(), "The method toSQL() does not return the expected string");
     }
 
 }
