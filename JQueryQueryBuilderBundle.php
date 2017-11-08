c56a867985ae62be844079fc6f410392fd2fc8e6
webeweb <support@webeweb.fr>
2017-11-08 14:01:52 +0100

    Remove WBW in bundle classes

---
+++ JQueryQueryBuilderBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2017 NdC/WBW
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * jQuery QueryBuilder bundle.
+ *
+ * @author NdC/WBW <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle
+ * @final
+ */
+final class JQueryQueryBuilderBundle extends Bundle {
+
+}
