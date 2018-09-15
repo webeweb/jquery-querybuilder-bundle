012f023338eecd23ade5899063e254a1f389ce83
webeweb <support@webeweb.fr>
2018-09-15 14:34:58 +0200

    Add an abstract jQuery QueryBuilder framework test case

---
+++ Tests/Cases/AbstractJQueryQueryBuilderFrameworkTestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Disclaimer: This source code is protected by copyright law and by
+ * international conventions.
+ *
+ * Any reproduction or partial or total distribution of the source code, by any
+ * means whatsoever, is strictly forbidden.
+ *
+ * Anyone not complying with these provisions will be guilty of the offense of
+ * infringement and the penal sanctions provided for by law.
+ *
+ * © 2018 All rights reserved.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AbstractJQueryQueryBuilderFrameworkTestCase.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Cases
+ */
+abstract class AbstractJQueryQueryBuilderFrameworkTestCase extends TestCase {
+
+}
