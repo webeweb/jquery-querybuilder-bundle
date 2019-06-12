abccad32bb95e978bae77f984f98fa9949019f49
webeweb <support@webeweb.fr>
2019-06-12 20:50:47 +0200

    Add QueryBuilder interface

---
+++ Tests/QueryBuilderInterfaceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\QueryBuilderInterface;
+
+/**
+ * QueryBuilder interface test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests
+ */
+class QueryBuilderInterfaceTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $this->assertEquals("2.4.4", QueryBuilderInterface::QUERYBUILDER_VERSION);
+    }
+}
