74d77dedd0110599367b3998409d7c77824e7fc5
webeweb <support@webeweb.fr>
2019-06-14 18:58:56 +0200

    Add Is not empty QueryBuilder operator

---
+++ Tests/Decorator/Operator/IsNotEmptyQueryBuilderOperatorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderOperatorInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator\IsNotEmptyQueryBuilderOperator;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Is not empty QueryBuilder operator test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator
+ */
+class IsNotEmptyQueryBuilderOperatorTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new IsNotEmptyQueryBuilderOperator();
+
+        $this->assertEquals(QueryBuilderOperatorInterface::OPERATOR_IS_NOT_EMPTY, $obj->getOperator());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setField("field");
+
+        $obj = new IsNotEmptyQueryBuilderOperator();
+
+        $this->assertEquals("field IS NOT NULL", $obj->toSQL($rule));
+    }
+}
