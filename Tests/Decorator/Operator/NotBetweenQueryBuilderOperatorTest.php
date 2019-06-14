f92ed6c3349a101e8ff417195cece16e31e8d4c7
webeweb <support@webeweb.fr>
2019-06-14 19:00:16 +0200

    Add Not between QueryBuilder operator

---
+++ Tests/Decorator/Operator/NotBetweenQueryBuilderOperatorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderOperatorInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator\NotBetweenQueryBuilderOperator;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Not between QueryBuilder operator test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator
+ */
+class NotBetweenQueryBuilderOperatorTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NotBetweenQueryBuilderOperator();
+
+        $this->assertEquals(QueryBuilderOperatorInterface::OPERATOR_NOT_BETWEEN, $obj->getOperator());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setField("field");
+        $rule->setType(QueryBuilderTypeInterface::TYPE_STRING);
+        $rule->setValue(["value1", "value2"]);
+
+        $obj = new NotBetweenQueryBuilderOperator();
+
+        $this->assertEquals("field NOT BETWEEN 'value1' AND 'value2'", $obj->toSQL($rule));
+    }
+}
