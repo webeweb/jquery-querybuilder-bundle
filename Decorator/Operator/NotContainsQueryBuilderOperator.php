467fdb183a167ad9945c353f3baad0d5e2fc54a0
webeweb <support@webeweb.fr>
2019-06-14 19:00:33 +0200

    Add Not contains QueryBuilder operator

---
+++ Decorator/Operator/NotContainsQueryBuilderOperator.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\QueryBuilderDecoratorFactory;
+
+/**
+ * Not contains QueryBuilder operator.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator
+ */
+class NotContainsQueryBuilderOperator extends AbstractQueryBuilderOperator {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::OPERATOR_NOT_CONTAINS);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSQL(QueryBuilderRuleInterface $rule, $wrap = false) {
+
+        $qbt = QueryBuilderDecoratorFactory::newQueryBuilderType($rule->getType());
+
+        $sql = [
+            parent::toSQL($rule, $wrap),
+            "'%" . $qbt->toSQL($rule, false) . "%'",
+        ];
+
+        return implode(" ", $sql);
+    }
+}
