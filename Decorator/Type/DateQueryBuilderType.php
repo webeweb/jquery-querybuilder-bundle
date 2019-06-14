35ba92b046ae72db0e89edcc14d00e5180af8803
webeweb <support@webeweb.fr>
2019-06-14 19:02:05 +0200

    Add Date QueryBuilder type

---
+++ Decorator/Type/DateQueryBuilderType.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
+
+/**
+ * Date QueryBuilder type.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type
+ */
+class DateQueryBuilderType extends AbstractQueryBuilderType {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::TYPE_DATE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSQL(QueryBuilderRuleInterface $rule, $wrap = false) {
+        return true === $wrap ? "'" . addslashes($rule->getValue()) . "'" : addslashes($rule->getValue());
+    }
+}
---
+++ Tests/Decorator/Type/DateQueryBuilderTypeTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type\DateQueryBuilderType;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Date QueryBuilder type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type
+ */
+class DateQueryBuilderTypeTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DateQueryBuilderType();
+
+        $this->assertEquals(QueryBuilderTypeInterface::TYPE_DATE, $obj->getType());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setValue("2019-06-14");
+
+        $obj = new DateQueryBuilderType();
+
+        $this->assertEquals("2019-06-14", $obj->toSQL($rule));
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQLWithWrap() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setValue("2019-06-14");
+
+        $obj = new DateQueryBuilderType();
+
+        $this->assertEquals("'2019-06-14'", $obj->toSQL($rule, true));
+    }
+}
