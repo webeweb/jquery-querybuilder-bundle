96eeeb71103bc3330d90229eacd2b0c3948c7112
webeweb <support@webeweb.fr>
2019-06-14 19:00:01 +0200

    Add Not begins QueryBuilder operator

---
+++ Tests/Decorator/Operator/NotBeginsWithQueryBuilderOperatorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderOperatorInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator\NotBeginsWithQueryBuilderOperator;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Not begins with QueryBuilder operator test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Operator
+ */
+class NotBeginsWithQueryBuilderOperatorTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new NotBeginsWithQueryBuilderOperator();
+
+        $this->assertEquals(QueryBuilderOperatorInterface::OPERATOR_NOT_BEGINS_WITH, $obj->getOperator());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setField("field");
+        $rule->setType(QueryBuilderRule::TYPE_STRING);
+        $rule->setValue("value");
+
+        $obj = new NotBeginsWithQueryBuilderOperator();
+
+        $this->assertEquals("field NOT LIKE 'value%'", $obj->toSQL($rule));
+    }
+}
