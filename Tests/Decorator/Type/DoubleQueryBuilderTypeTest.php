532e8d86ca561ce251f9b44e4a029180311a3215
webeweb <support@webeweb.fr>
2019-06-14 19:02:27 +0200

    Add Double QueryBuilder type

---
+++ Tests/Decorator/Type/DoubleQueryBuilderTypeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type\DoubleQueryBuilderType;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Double QueryBuilder type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type
+ */
+class DoubleQueryBuilderTypeTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new DoubleQueryBuilderType();
+
+        $this->assertEquals(QueryBuilderTypeInterface::TYPE_DOUBLE, $obj->getType());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setValue(5.5);
+
+        $obj = new DoubleQueryBuilderType();
+
+        $this->assertEquals(5.5, $obj->toSQL($rule));
+    }
+}
