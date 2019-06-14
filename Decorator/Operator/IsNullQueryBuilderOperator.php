ede8d4de0a4b4c4413085bf40d458b0a194eea79
webeweb <support@webeweb.fr>
2019-06-14 18:59:21 +0200

    Add Is null QueryBuilder operator

---
+++ Decorator/Operator/IsNullQueryBuilderOperator.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
+
+/**
+ * Is null QueryBuilder operator.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Operator
+ */
+class IsNullQueryBuilderOperator extends AbstractQueryBuilderOperator {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::OPERATOR_IS_NULL);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSQL(QueryBuilderRuleInterface $rule, $wrap = false) {
+        return parent::toSQL($rule, $wrap);
+    }
+}
