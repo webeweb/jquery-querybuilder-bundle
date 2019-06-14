93a1db36162e906d579ab5d6ce6640009b8a674b
webeweb <support@webeweb.fr>
2019-06-14 19:01:52 +0200

    Add Boolean QueryBuilder type

---
+++ Decorator/Type/BooleanQueryBuilderType.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
+use WBW\Library\Core\Argument\IntegerHelper;
+
+/**
+ * Boolean QueryBuilder type.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type
+ */
+class BooleanQueryBuilderType extends AbstractQueryBuilderType {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(self::TYPE_BOOLEAN);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSQL(QueryBuilderRuleInterface $rule, $wrap = false) {
+        return IntegerHelper::parseBoolean($rule->getValue());
+    }
+}
---
+++ Tests/Decorator/Type/BooleanQueryBuilderTypeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type\BooleanQueryBuilderType;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Boolean QueryBuilder type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type
+ */
+class BooleanQueryBuilderTypeTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new BooleanQueryBuilderType();
+
+        $this->assertEquals(QueryBuilderTypeInterface::TYPE_BOOLEAN, $obj->getType());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setValue(true);
+
+        $obj = new BooleanQueryBuilderType();
+
+        $this->assertEquals("1", $obj->toSQL($rule));
+    }
+}
