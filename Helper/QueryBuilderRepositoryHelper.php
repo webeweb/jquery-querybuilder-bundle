46341aa2630ed368cf6e2ec26275954b11c0f999
webeweb <support@webeweb.fr>
2019-06-14 19:05:32 +0200

    Implements QueryBuilder decorator factory

--- Helper/QueryBuilderRepositoryHelper.php
+++ Helper/QueryBuilderRepositoryHelper.php
@@ -12,12 +12,11 @@
 namespace WBW\Bundle\JQuery\QueryBuilderBundle\Helper;
 
 use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderConditionInterface;
-use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderEnumerator;
 use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderOperatorInterface;
 use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
 use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleSetInterface;
 use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
-use WBW\Library\Core\Argument\IntegerHelper;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\QueryBuilderDecoratorFactory;
 
 /**
  * QueryBuilder repository helper.
@@ -35,55 +34,12 @@ class QueryBuilderRepositoryHelper implements QueryBuilderConditionInterface, Qu
      */
     public static function queryBuilderRule2SQL(QueryBuilderRuleInterface $rule) {
 
-        $sql   = [];
-        $sql[] = $rule->getField();
-        $sql[] = QueryBuilderEnumerator::enumOperators()[$rule->getOperator()];
-
-        switch ($rule->getOperator()) {
-
-            case self::OPERATOR_BEGINS_WITH:
-            case self::OPERATOR_NOT_BEGINS_WITH:
-                $sql[] = "'" . static::quoteMixedValue($rule->getType(), $rule->getValue(), false) . "%'";
-                break;
-
-            case self::OPERATOR_BETWEEN:
-            case self::OPERATOR_NOT_BETWEEN:
-                $sql[] = implode(" " . self::CONDITION_AND . " ", static::quoteArrayValue($rule->getType(), $rule->getValue(), true));
-                break;
-
-            case self::OPERATOR_CONTAINS:
-            case self::OPERATOR_NOT_CONTAINS:
-                $sql[] = "'%" . static::quoteMixedValue($rule->getType(), $rule->getValue(), false) . "%'";
-                break;
-
-            case self::OPERATOR_ENDS_WITH:
-            case self::OPERATOR_NOT_ENDS_WITH:
-                $sql[] = "'%" . static::quoteMixedValue($rule->getType(), $rule->getValue(), false) . "'";
-                break;
-
-            case self::OPERATOR_EQUAL:
-            case self::OPERATOR_GREATER:
-            case self::OPERATOR_GREATER_OR_EQUAL:
-            case self::OPERATOR_LESS:
-            case self::OPERATOR_LESS_OR_EQUAL:
-            case self::OPERATOR_NOT_EQUAL:
-                $sql[] = static::quoteMixedValue($rule->getType(), $rule->getValue(), true);
-                break;
-
-            case self::OPERATOR_IN:
-            case self::OPERATOR_NOT_IN:
-                $sql[] = "(" . implode(", ", static::quoteArrayValue($rule->getType(), $rule->getValue(), true)) . ")";
-                break;
-
-            case self::OPERATOR_IS_EMPTY:
-            case self::OPERATOR_IS_NOT_EMPTY:
-            case self::OPERATOR_IS_NOT_NULL:
-            case self::OPERATOR_IS_NULL:
-                // NOTHING TO DO.
-                break;
+        $qbo = QueryBuilderDecoratorFactory::newQueryBuilderOperator($rule->getOperator());
+        if (null === $qbo) {
+            return "";
         }
 
-        return implode(" ", $sql);
+        return $qbo->toSQL($rule, false);
     }
 
     /**
@@ -114,55 +70,5 @@ class QueryBuilderRepositoryHelper implements QueryBuilderConditionInterface, Qu
         }
 
         return "(" . implode(" " . $ruleSet->getCondition() . " ", $sql) . ")";
-    }
-
-    /**
-     * Quote an array of values.
-     *
-     * @param string $type The type.
-     * @param mixed $value The value.
-     * @param bool $wrap Wrap ?
-     * @return array Returns the quoted values.
-     */
-    protected static function quoteArrayValue($type, $value, $wrap = false) {
-        $output = [];
-        foreach ($value as $current) {
-            $output[] = static::quoteMixedValue($type, $current, $wrap);
-        }
-        return $output;
-    }
-
-    /**
-     * Quote a mixed value.
-     *
-     * @param string $type The type.
-     * @param mixed $value The value.
-     * @param bool $wrap Wrap ?
-     * @return mixed|null Returns the quoted value in case of success, null otherwise.
-     */
-    protected static function quoteMixedValue($type, $value, $wrap = false) {
-
-        $output = null;
-
-        switch ($type) {
-
-            case self::TYPE_BOOLEAN:
-                $output = IntegerHelper::parseBoolean($value);
-                break;
-
-            case self::TYPE_DATE:
-            case self::TYPE_DATETIME:
-            case self::TYPE_STRING:
-            case self::TYPE_TIME:
-                $output = true === $wrap ? "'" . addslashes($value) . "'" : addslashes($value);
-                break;
-
-            case self::TYPE_DOUBLE:
-            case self::TYPE_INTEGER:
-                $output = $value;
-                break;
-        }
-
-        return $output;
     }
 }
