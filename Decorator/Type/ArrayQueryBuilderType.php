e2ba6a2f038e6cd57daa9d8a34e9c04660b01e63
webeweb <support@webeweb.fr>
2019-06-14 19:03:15 +0200

    Add Array QueryBuilder type

---
+++ Decorator/Type/ArrayQueryBuilderType.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRuleInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\QueryBuilderDecoratorFactory;
+
+/**
+ * Array QueryBuilder type.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type
+ */
+class ArrayQueryBuilderType extends AbstractQueryBuilderType {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function toSQL(QueryBuilderRuleInterface $rule, $wrap = false) {
+
+        $sql = [];
+
+        foreach ($rule->getValue() as $current) {
+
+            $qbd = QueryBuilderDecoratorFactory::newQueryBuilderType($rule->getType());
+            if (null === $qbd) {
+                continue;
+            }
+
+            $qbr = (new QueryBuilderRule())->setType($rule->getType())->setValue($current);
+
+            $sql[] = $qbd->toSQL($qbr, $wrap);
+        }
+
+        return $sql;
+    }
+}
---
+++ Tests/Decorator/Type/ArrayQueryBuilderTypeTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the jquery-querybuilder-bundle package.
+ *
+ * (c) 2019 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type;
+
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderRule;
+use WBW\Bundle\JQuery\QueryBuilderBundle\API\QueryBuilderTypeInterface;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Decorator\Type\ArrayQueryBuilderType;
+use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractTestCase;
+
+/**
+ * Array QueryBuilder type test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Decorator\Type
+ */
+class ArrayQueryBuilderTypeTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function testConstruct() {
+
+        $obj = new ArrayQueryBuilderType();
+
+        $this->assertNull($obj->getType());
+    }
+
+    /**
+     * Tests the toSQL() method.
+     *
+     * @return void
+     */
+    public function testToSQL() {
+
+        // Set a QueryBuilder rule mock.
+        $rule = new QueryBuilderRule();
+        $rule->setType(QueryBuilderTypeInterface::TYPE_INTEGER);
+        $rule->setValue([0, 1]);
+
+        $obj = new ArrayQueryBuilderType();
+
+        $this->assertEquals([0, 1], $obj->toSQL($rule));
+    }
+}
