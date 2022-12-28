1c7b2b09d8fc34c7cbb4542db4562393da3cdea7
webeweb <support@webeweb.fr>
2022-12-28 10:42:39 +0100

    Improve functional tests

--- Tests/Controller/ViewsControllerTest.php
+++ Tests/Controller/ViewsControllerTest.php
@@ -14,25 +14,25 @@ namespace WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Controller;
 use WBW\Bundle\JQuery\QueryBuilderBundle\Tests\AbstractWebTestCase;
 
 /**
- * Layout controller test.
+ * Views controller test.
  *
  * @author webeweb <https://github.com/webeweb>
  * @package WBW\Bundle\JQuery\QueryBuilderBundle\Tests\Controller
  */
-class LayoutControllerTest extends AbstractWebTestCase {
+class ViewsControllerTest extends AbstractWebTestCase {
 
     /**
-     * Tests javascriptsAction()
+     * Tests assetsJavascriptsAction()
      *
      * @return void
      */
-    public function testJavascriptsAction(): void {
+    public function testAssetsJavascriptsAction(): void {
 
         // Create a client.
         $client = $this->client;
 
         // Make a GET request.
-        $client->request("GET", "/javascripts");
+        $client->request("GET", "/assets/javascripts");
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals("text/html; charset=UTF-8", $client->getResponse()->headers->get("Content-Type"));
 
@@ -46,17 +46,17 @@ class LayoutControllerTest extends AbstractWebTestCase {
     }
 
     /**
-     * Tests stylesheetsAction()
+     * Tests assetsStylesheetsAction()
      *
      * @return void
      */
-    public function testStylesheetsAction(): void {
+    public function testAssetsStylesheetsAction(): void {
 
         // Create a client.
         $client = $this->client;
 
         // Make a GET request.
-        $client->request("GET", "/stylesheets");
+        $client->request("GET", "/assets/stylesheets");
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals("text/html; charset=UTF-8", $client->getResponse()->headers->get("Content-Type"));
 
